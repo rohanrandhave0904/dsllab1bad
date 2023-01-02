39d57d8b5df5293936de96fbab32a27c9d9c4971
rohanrandhave0904 <121605792+rohanrandhave0904@users.noreply.github.com>
2023-01-02 16:15:21 +0530

    Add files via upload

---
+++ quick.py
@@ -0,0 +1,26 @@
+def quicksort(arr,left,right):
+    if left<right:
+        partition_pos=partition(arr,left,right)
+        quicksort(arr,left,partition_pos-1)
+        quicksort(arr,partition_pos+1,right)
+
+def partition(arr,left,right):
+    i=left
+    j=right-1
+    pivot=arr[right]
+    while i<j:
+        while i<right and arr[i]<pivot:
+            i=i+1
+        while j>left and arr[j]>pivot:
+            j=j-1
+        if i<j:
+            arr[i],arr[j]=arr[j],arr[i]
+    if arr[i]>pivot:
+        arr[i],arr[right]=arr[right],arr[i]
+    return i
+
+arr=[11,44,22,33,77,88,56,34]
+quicksort(arr,0,len(arr)-1)
+print(arr)
+
+
